a6a5bdcdf858670d65c51af10220a3fc318a1dd3
pontakornth <pontakorn61@gmail.com>
2020-11-16 14:15:50 +0700

    fix: Incorrect theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-    extend: {
-        colors: {
-            primary: "#283B7C",
-            secondary: "#24C200",
-            menu: "#A1A1A1",
-            "text-gray": "#C2C2C2",
-            "text-black": "#595959"
-
+    theme: {
+        extend: {
+            colors: {
+                primary: "#283B7C",
+                secondary: "#24C200",
+                menu: "#A1A1A1",
+                "text-gray": "#C2C2C2",
+                "text-black": "#595959"
+            }
         }
     }
 }
