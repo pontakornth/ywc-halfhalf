7ebb568b63331025752d7eb8fbfa1354668662fd
pontakornth <pontakorn61@gmail.com>
2020-11-16 12:38:46 +0700

    feat: Add color

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extend: {
+        colors: {
+            primary: "#283B7C",
+            secondary: "#24C200",
+            menu: "#A1A1A1",
+            "text-gray": "#C2C2C2",
+            "text-black": "#595959"
+
+        }
+    }
+}
