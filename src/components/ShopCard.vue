83594014643a841c6be5a21ab54ba446bffde8bf
Pontakorn Paesaeng <pontakorn61@gmail.com>
2020-11-17 18:01:22 +0700

    feat: Prototype for card component

---
+++ src/components/ShopCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="Card">
+        <div class="CardHeader">
+            <img class="CardImage" :src="imageUrl">
+            <div class="CardHeaderText">
+                <h3 class="CardTitle">{{ title }}</h3>
+                <span v-if="tag">{{ tag }}</span>
+                <div class="CardInfo">
+                    <span class="CardInfoItem">{{ category }} </span>
+                    <span v-if="priceRange" class="CardInfoItem"> {{ priceRange }} </span>
+                    <span class="CardInfoItem"> {{ location }} </span>
+                </div>
+            </div>
+        </div>
+        <div class="CardDescription">
+            <p class="CardDescrptionText"> {{ description }}</p>
+            <span>เมนูแนะนำ:</span> {{ recommendation }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ShopCard"
+}
+</script>
