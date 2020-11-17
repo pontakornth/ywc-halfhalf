e63cb2206af84d0c8f2d5178c8fc4de02296356c
pontakornth <pontakorn61@gmail.com>
2020-11-17 12:33:18 +0700

    refactor: Separate Navigation

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
    <div class="Page">
      <search-bar/>
+     <navigation/>
    </div>
 </template>
 
 <script>
 import SearchBar from './components/SearchBar.vue';
 import "../node_modules/@ibm/plex/css/ibm-plex.css";
+import Navigation from './components/Navigation.vue';
 
 export default {
   name: 'App',
   components: {
-    SearchBar
+    SearchBar,
+    Navigation
   }
 }
 </script>
