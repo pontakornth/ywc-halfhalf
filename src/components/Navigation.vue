f3353efecc9faea3360d727cf06598142ad60aa7
pontakornth <pontakorn61@gmail.com>
2020-11-20 13:59:43 +0700

    styling: Add container

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,9 +1,11 @@
 <template>
-     <nav class="Navigation">
+    <div class="bg-primary">
+     <nav class="Navigation container">
        <a class="underline font-medium" href="/">หน้าแรก</a>
        <span>/</span>
        <span class="font-bold">ค้นหา</span>
      </nav>
+    </div>
 </template>
 
 <script>
@@ -14,7 +16,7 @@ export default {
 
 <style lang="postcss" scoped>
 .Navigation {
-  @apply py-3 px-3 flex bg-primary text-white;
+  @apply py-3 px-3 flex bg-primary text-white mx-auto;
 }
 .Navigation * {
   @apply mr-3 ;
